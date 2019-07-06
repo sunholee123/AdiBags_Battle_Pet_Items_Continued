42d12a2fe6361af2bd6451a7ab04b77436063b4a
Lown Ignitus <lownignitus@gmail.com>
2019-07-06 15:40:02 -0700

    v1.0.5
    Added koKR Localization from Comiluv

--- AdiBags_Battle_Pet_Items.lua
+++ AdiBags_Battle_Pet_Items.lua
@@ -1,7 +1,7 @@
 --[[
 AdiBags - Battle Pet Items
 by LownIgnitus
-version: v1.0.04
+version: v1.0.5
 Add various Battle Pet items to AdiBags filter groups
 ]]
 
--- Localization.lua
+++ Localization.lua
@@ -42,7 +42,25 @@ elseif locale == "frFR" then
 elseif locale == "itIT" then
 --Translation missing
 elseif locale == "koKR" then
---Translation missing
+	-- From Comiluv
+L["Battle Pet Items"] = "전투 애완동물 물품"
+L["Items that are connected to Battle Pets and not actual pets."] = "전투 애완동물과 관련이 있지만 실제 전투 애완동물이 아닌 물품."
+L["Miscellaneous Items"] = "기타 물품"
+L["Items that dont fall into any other categorys"] = "다른 분류에 속하지 않는 물품"
+L["Battle Pet Currency Items"] = "전투 애완동물 화폐"
+L["Items used to buy Battle Pet related Items"] = "전투 애완동물 관련 상품을 사는데 필요한 물품"
+L["Drop Battle Pet containers"] = "전투 애완동물 상자를 생성"
+L["Items that drop that can contain Battle Pets"] = "전투 애완동물을 포함할 수 있는 물품"
+L["Bags and Supplies"] = "가방과 도구들"
+L["Bags that are obtained that contain Battle Pet Items"] = "전투 애완동물 물품을 담은 가방"
+L["Rarity Stones"] = "훈련석"
+L["Items that increase rarity of Battle Pets"] = "전투 애완동물의 등급를 상승하는 물품"
+L["Training Stones"] = "전투석"
+L["Items that add levels to Battle Pets"] = "전투 애완동물의 레벨을 상승하는 물품"
+L["Pet Toys"] = "전투 애완동물 장난감"
+L["Toys usable with all Battle Pets"] = "모든 전투 애완동물에 사용할 수 있는 물품"
+L["Pug Costumes"] = "막공이 의상"
+L["All items for your Perky Pugs"] = "명랑한 막공이가 사용할 수 있는 물품"
 elseif locale == "ptBR" then
 --Translation missing
 elseif locale == "ruRU" then
